bb06179861f33c1269bc6d4e2b9e2195cb320d2f
princess <princessmumbi001@gmail.com>
2024-05-15 10:17:06 +0300

    states and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,11 @@ import itemData from "../data/items";
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
   const [appClass, setAppClass] = useState(true)
 
   const handleClick = ()=>{
     setAppClass(!appClass)
   }
-
 
   return (
     <div onClick={handleClick} className={appClass ? "App light":"App dark"}>
